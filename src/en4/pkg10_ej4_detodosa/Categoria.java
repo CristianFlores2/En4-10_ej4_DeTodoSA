cbfe7e7cf26802b885fbb2a285df622bb55adb43
CristianFlores2 <crisf5858@gmail.com>
2023-08-14 17:49:14 -0300

    Initial commit

---
+++ src/en4/pkg10_ej4_detodosa/Categoria.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package en4.pkg10_ej4_detodosa;
+
+/**
+ *
+ * @author crist
+ */
+public enum Categoria {
+    Comestible,Limpieza,Perfumeria
+}
---
+++ src/en4/pkg10_ej4_detodosa/En410_ej4_DeTodoSA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package en4.pkg10_ej4_detodosa;
+
+/**
+ *
+ * @author crist
+ */
+public class En410_ej4_DeTodoSA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
